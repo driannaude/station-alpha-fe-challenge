28dfd9e0cf8b79a2bf56f5e9ac6949e16ede13f5
Drian Naude <drian.naude@gmail.com>
2025-09-22 10:38:26 +1200

    fix: add defined component props to TodoFilter

--- apps/bug-hunting/src/components/TodoFilter.tsx
+++ apps/bug-hunting/src/components/TodoFilter.tsx
@@ -1,38 +1,48 @@
-const TodoFilter = ({ filter, onFilter, onClearCompleted }) => {
-  const handleFilterChange = (newFilter) => {
-    onFilter(newFilter)
-  }
-  
+import { FC } from "react";
+import { Filter } from "../types/filter.types";
+
+export interface TodoFilterProps {
+  filter: Filter;
+  onFilter: (filter: Filter) => void;
+  onClearCompleted: () => void;
+}
+
+const TodoFilter: FC<TodoFilterProps> = ({
+  filter,
+  onFilter,
+  onClearCompleted,
+}) => {
+  const handleFilterChange = (newFilter: Filter) => {
+    onFilter(newFilter);
+  };
+
   return (
     <div className="todo-filter">
       <div className="filter-buttons">
-        <button 
-          className={filter === "all" && "active"} 
+        <button
+          className={filter === "all" && "active"}
           onClick={() => handleFilterChange("all")}
         >
           All
         </button>
-        <button 
-          className={`${filter === active ? "active" : ""}`} 
+        <button
+          className={`${filter === active ? "active" : ""}`}
           onClick={() => handleFilterChange("active")}
         >
           Active
         </button>
-        <button 
-          className={filter == "completed" ? "active" : ""} 
+        <button
+          className={filter == "completed" ? "active" : ""}
           onClick={() => handleFilterChange("completed")}
         >
           Completed
         </button>
       </div>
-      
-      <button 
-        className="clear-completed"
-        onClick={onClearCompleted()}
-      >
+
+      <button className="clear-completed" onClick={onClearCompleted()}>
         Clear completed
       </button>
     </div>
-  )
-}
-export default TodoFilter 
+  );
+};
+export default TodoFilter;
