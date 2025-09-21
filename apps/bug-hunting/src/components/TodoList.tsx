41579be7f97bba321f99f32e722bc55a0d9f8902
Drian Naude <drian.naude@gmail.com>
2025-09-22 10:30:31 +1200

    fix: add prop types for TodoList component

--- apps/bug-hunting/src/components/TodoList.tsx
+++ apps/bug-hunting/src/components/TodoList.tsx
@@ -1,24 +1,30 @@
-const TodoList = ({ todos, onToggle, onDelete }) => {
+import { FC } from "react";
+import { Todo } from "../types/todo.types";
+
+export interface TodoListProps {
+  todos: Todo[];
+  onToggle: (id: string) => void;
+  onDelete: (id: string) => void;
+}
+
+const TodoList: FC<TodoListProps> = ({ todos, onToggle, onDelete }) => {
   return (
     <ul className="todo-list">
       {todos.map((todo) => (
-        <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>
-          <input 
-            type="checkbox" 
-            checked={todo.complete} 
+        <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+          <input
+            type="checkbox"
+            checked={todo.complete}
             onChange={() => onToggle(todo)}
           />
           <span>{todo.text}</span>
-          <button 
-            className="delete-btn"
-            onClick={onDelete(todo.id)}
-          >
+          <button className="delete-btn" onClick={onDelete(todo.id)}>
             Delete
           </button>
         </li>
       ))}
     </ul>
-  )
-}
+  );
+};
 
-export default TodoList 
+export default TodoList;
